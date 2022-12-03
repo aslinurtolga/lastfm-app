1cd4178153902f8c438b5cf82771fadbea328ec3
AslinurTolga <aslinurtolga@gmail.com>
2022-12-03 23:23:06 +0300

    feat: card component is completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const TopCards = ({ item, name }) => {
+const Card = ({ item, name }) => {
   return (
-    <div className="flex border border-white rounded-md">
-      <img className="w-1/3" src={item.image[3]["#text"]} alt="imageArtist" />
-      <div className="flex flex-col justify-start items-center">
-        <h5 className="text-sm md:text-base font-bold py-2 uppercase">
+    <div className="flex border border-white rounded-md h-40">
+      <img className="w-1/3 rounded-md" src={item.image[3]["#text"]} alt="imageArtist" />
+      <div className="flex flex-col justify-center items-center w-full text-center">
+        <h5 className="text-sm md:text-base font-bold py-2 uppercase truncate w-32 ">
           {item?.name}
         </h5>
         <p className="text-xs md:text-sm italic py-2">{name}</p>
@@ -16,4 +16,4 @@ const TopCards = ({ item, name }) => {
   );
 };
 
-export default TopCards;
+export default Card;
--- src/pages/ArtistDetail.jsx
+++ src/pages/ArtistDetail.jsx
@@ -34,24 +34,24 @@ const ArtistDetail = () => {
   }, []);
 
   return (
-    <div className="bg-gradient-to-b overflow-hidden h-full from-yellow-500 via-purple-500 to-blue-500 dark:from-dark-700 dark:to-gray-900">
-      <div className="flex sm:w-1/3 w-2/3 mx-auto my-5">
+    <div className="bg-gradient-to-b h-full overflow-hidden from-yellow-500 via-purple-500 to-blue-500 dark:from-dark-700 dark:to-gray-900">
+      <div className="flex border-4 sm:w-1/3 w-2/3 mx-auto my-5">
         <img className="flex w-1/4" src={tracks[0]?.image[2]["#text"]} alt="" />
         <div className="flex flex-col justify-center w-full items-center py-3 px-1">
           <h1 className="text-base font-semibold uppercase sm:text-2xl">
             {name}
           </h1>
+          <p className="italic py-1">The Singer</p>
         </div>
-        <p className="italic py-1">The Singer</p>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-between items-start w-2/3 mx-auto h-2/3 overflow-auto aslinur">
+      <div className="flex flex-col md:flex-row justify-between items-start w-3/4 mx-auto h-2/3 overflow-auto aslinur">
         {/* ALBUMS */}
-        <div className="flex flex-col items-center md:w-1/2 h-1/2 md:mr-6 sm:h-full overflow-auto aslinur ">
-          <h2 className="font-bold text-2xl p-4 uppercase italic ">
+        <div className="flex flex-col items-center md:w-1/2 h-1/2 md:mr-6 sm:h-full overflow-auto aslinur">
+          <h2 className="font-bold text-2xl p-4 uppercase italic">
             Top Albums
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {albums?.map((item, index) => (
               <Card key={index} item={item} name={name} />
             ))}
@@ -59,11 +59,11 @@ const ArtistDetail = () => {
         </div>
 
         {/* TRACKS */}
-        <div className="flex flex-col items-center md:w-1/2 md:ml-6 h-1/2 sm:h-full overflow-auto aslinur">
+        <div className="flex flex-col items-center md:w-1/2 mt-10 md:mt-0 h-1/2 md:ml-6 sm:h-full overflow-auto aslinur">
           <h2 className="font-bold text-2xl p-4 uppercase italic ">
             Top Tracks
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {tracks?.map((item, index) => (
               <Card key={index} item={item} name={name} />
             ))}
