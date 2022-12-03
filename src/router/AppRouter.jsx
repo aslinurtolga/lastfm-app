fce9051b38b5bc96828ba54bfa9c1eef80cef7f4
AslinurTolga <aslinurtolga@gmail.com>
2022-12-03 14:29:43 +0300

    feat: added style to homepage

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,16 +4,15 @@ import Header from "../components/Header";
 import ArtistDetail from "../pages/ArtistDetail";
 import Home from "../pages/Home";
 
-
 const AppRouter = () => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="artistDetail/:name" element={<ArtistDetail />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
