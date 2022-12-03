dbfb3da1c3054ed14825be350b7775b2a09ba33d
AslinurTolga <aslinurtolga@gmail.com>
2022-12-03 14:03:31 +0300

    feat: artist names added

--- src/components/TopArtistList.jsx
+++ src/components/TopArtistList.jsx
@@ -5,7 +5,13 @@ const TopArtistList = ({artistList}) => {
   return (
    <Link to={`artistDetail/${artistList.name}`}>
     <div className="">
-        <img src={artistList?.image[2]["#text"]} alt="" className="" />
+        <img src={artistList?.image[2]["#text"]} alt="artistImg" className="" />
+    </div>
+    <div>
+        <div>
+            <h4>Artist</h4>
+            <p>{artistList?.name}</p>
+        </div>
     </div>
    </Link>
   )
